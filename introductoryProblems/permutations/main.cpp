53fd30e4f016d781d39e3d31a167501ac87ab73f
Rohan Dwi Cahyono <rohandwi689@gmail.com>
2026-02-20 07:47:50 +0700

    update sederhana

--- introductoryProblems/permutations/main.cpp
+++ introductoryProblems/permutations/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
 
-    for(int i = 0; i <= 1; i--) {
-        i = n;
-        cout << n << endl;
-        n--;
+    vector<long long> arr(n); // Deklarasi vector
+
+    int index = 0;
+    // Loop genap
+    for (int i = 2; i <= n; i += 2) {
+        arr[index] = i;
+        index++;
     }
+
+    // Loop ganjil
+    for (int i = 1; i <= n; i += 2) {
+        arr[index] = i;
+        index++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
